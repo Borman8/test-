5ef498950e245c51ca59b43cea2f7a463f90af53
Borman8 <alanshuleikin@gmail.com>
2019-10-19 13:42:22 +0300

    fix v2

--- Taks 1/pr_triangle.rb
+++ Taks 1/pr_triangle.rb
@@ -1,33 +1,31 @@
-puts "Введите значение стороны 1"
+puts 'Введите значение стороны 1'
 side_one = gets.chomp.to_i
-puts "Введите значение стороны 2"
+puts 'Введите значение стороны 2'
 side_two = gets.chomp.to_i
-puts "Введите значение стороны 3"
+puts 'Введите значение стороны 3'
+
 side_three = gets.chomp.to_i
 # Проверка треугольника 
 if side_one == side_two && side_one == side_three
- 	puts "Треугольник равносторонний и его стороны равны #{side_one}"
+ 	puts 'Треугольник равносторонний и его стороны равны #{side_one}'
 elsif side_one == side_two || side_one == side_three
-	puts "Треугольник равнобедренный со значением равных стороно #{side_one}"
+	puts 'Треугольник равнобедренный со значением равных стороно #{side_one}'
 elsif side_two == side_three
-	puts "Треугольник равнобедренный со значением равных стороно #{side_two}"
+	puts 'Треугольник равнобедренный со значением равных стороно #{side_two}'
 elsif side_one > side_two && side_one > side_three
-	gipotinuza = side_one
- 	if gipotinuza**2 == side_two**2 + side_three**2
- 		puts "Треугольик прмямоугольный c гипотинузой #{side_one} и катетами #{side_two}, #{side_three}"
+	hypotenuse = side_one
+ 	if hypotenuse**2 == side_two**2 + side_three**2
+ 		puts 'Треугольик прмямоугольный c гипотинузой #{side_one} и катетами #{side_two}, #{side_three}'
  	end 
 elsif side_two > side_one && side_two > side_three
- 	gipotinuza = side_two
- 	if gipotinuza**2 == side_one**2 + side_three**2
- 		puts "Треугольик прмямоугольный c гипотинузой #{side_two} и катетами #{side_one}, #{side_three}"
+ 	hypotenuse = side_two
+ 	if hypotenuse**2 == side_one**2 + side_three**2
+ 		puts 'Треугольик прмямоугольный c гипотинузой #{side_two} и катетами #{side_one}, #{side_three}'
  	end 
 elsif
- 	gipotinuza = side_three
- 	if gipotinuza**2 == side_two**2 + side_one**2
- 		puts "Треугольик прмямоугольный c гипотинузой #{side_three} и катетами #{side_two}, #{side_one}"
+ 	hypotenuse = side_three
+ 	if hypotenuse**2 == side_two**2 + side_one**2
+ 		puts 'Треугольик прмямоугольный c гипотинузой #{side_three} и катетами #{side_two}, #{side_one}'
  	end 
-	puts "Обычный треугольник"
+	puts 'Обычный треугольник'
 end
-
-
-
--- Taks 1/wieght.rb
+++ Taks 1/wieght.rb
@@ -1,10 +1,11 @@
-puts "What is your name?"
+puts 'What is your name?'
 name_user = gets.chomp
-puts "Enter your Weight"
+puts 'Enter your Weight'
 weight_user = gets.chomp.to_i
 ideal_weight = weight_user - 110
+
 if ideal_weight < 0 
-	puts "#{name_user} your weight is #{weight_user} and it's ideal man"
+	puts '#{name_user} your weight is #{weight_user} and it's ideal man'
 else
-	puts "#{name_user} your weight is #{weight_user} and it's not normal man"
+	puts '#{name_user} your weight is #{weight_user} and it's not normal man'
 end
